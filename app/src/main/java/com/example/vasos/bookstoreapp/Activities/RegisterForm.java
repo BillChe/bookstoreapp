069a3cfd1b57cd114d4e7bc49a2585238956aef0
BillChe <cheimonisv@hotmail.com>
2020-05-28 23:00:44 +0300

    Commit with some first added code for APIS, added Volley library in gradle

--- app/src/main/java/com/example/vasos/bookstoreapp/Activities/RegisterForm.java
+++ app/src/main/java/com/example/vasos/bookstoreapp/Activities/RegisterForm.java
@@ -6,14 +6,31 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.vasos.bookstoreapp.DB.SQLiteDbHelper;
+import com.example.vasos.bookstoreapp.Helpers.AppConfig;
+import com.example.vasos.bookstoreapp.Helpers.AppController;
+import com.example.vasos.bookstoreapp.Helpers.SessionManager;
 import com.example.vasos.bookstoreapp.Models.AppUser;
 import com.example.vasos.bookstoreapp.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.support.constraint.Constraints.TAG;
 import static com.example.vasos.bookstoreapp.Activities.MainActivity.appUser;
 
 public class RegisterForm extends Activity {
@@ -21,12 +38,16 @@ public class RegisterForm extends Activity {
     private Context context;
     private EditText usernameEditText,passwordEditText, emailEditText;
     boolean registered;
+    private SessionManager session;
+    private SQLiteDbHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register_form);
+        // Session manager
+        session = new SessionManager(getApplicationContext());
 
         usernameEditText = findViewById(R.id.usernameET);
         passwordEditText = findViewById(R.id.passwordET);
@@ -34,7 +55,6 @@ public class RegisterForm extends Activity {
 
         context = this;
         registerFormButton = (Button) findViewById(R.id.registerFormButton);
-
     }
 
     @Override
@@ -44,14 +64,18 @@ public class RegisterForm extends Activity {
         registerFormButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
                 if((usernameEditText.getText()!=null) && (passwordEditText.getText()!=null))
                 {
+                    String username = usernameEditText.getText().toString().trim();
+                    String password = passwordEditText.getText().toString().trim();
+                    String email = emailEditText.getText().toString().trim();
+
+                    registerUser(username, password, email);
+
                     appUser = new AppUser(0,usernameEditText.getText().toString(),passwordEditText.getText().toString(),0, true);
-/*
+
                     SQLiteDbHelper sqLiteDbHelper= new SQLiteDbHelper(context);
-                    sqLiteDbHelper.insertUser(appUser);*/
+                    sqLiteDbHelper.insertUser(appUser);
 
 
                     new AlertDialog.Builder(context)
@@ -74,8 +98,6 @@ public class RegisterForm extends Activity {
 
                 registered = true;
 
-
-
             }
 
         });
@@ -88,5 +110,80 @@ public class RegisterForm extends Activity {
             this.finish();
         }
 
+    }
+    /**
+     * Function to store user in MySQL database will post params(tag, name,
+     * email, password) to register url
+     * */
+    private void registerUser(final String username, final String password, final String email) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_register";
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        // User successfully stored in MySQL
+                        // Now store the user in sqlite
+
+                        JSONObject user = jObj.getJSONObject("user");
+                        String username = user.getString("username");
+                        String uid = user.getString("id");
+                        String email = user.getString("email");
+
+                        // Inserting row in users table
+                        db.insertUser(appUser);
+
+                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+
+                        // Launch login activity
+
+                    } else {
+
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 }
---
+++ app/src/main/java/com/example/vasos/bookstoreapp/Helpers/AppConfig.java
@@ -0,0 +1,19 @@
+package com.example.vasos.bookstoreapp.Helpers;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://users.iee.ihu.gr/~it093504/boostore_api/login.php";
+    //http://users.iee.ihu.gr/~it093504/boostore_api/login.php?email=maniakas.nikoss@gmail.com&password=darklord
+    // Server user register url
+    public static String URL_REGISTER = "http://users.iee.ihu.gr/~it093504/boostore_api/signup.php";
+    //http://users.iee.ihu.gr/~it093504/boostore_api/signup.php?email=maniakas.nikoss@gmail.com&password=darklord&name=Nikos
+   /* // Server book info url
+    public static String URL_BOOK_INFO = "https://users.it.teithe.gr/~it093511/api/getBookInfo.php";
+
+    // Server book buy url
+    public static String URL_BOOK_BUY_INFO = "https://users.it.teithe.gr/~it093511/api/buyBook.php";
+    // search book url
+    public static String URL_SEARCH_BOOK_INFO = "https://users.it.teithe.gr/~it093511/api/searchBooks.php";
+
+    public static String URL_BOOKS_BUYED = "https://users.it.teithe.gr/~it093511/api/getBooksBuyed.php";*/
+}
